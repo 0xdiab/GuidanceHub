c2134a14a4738ad0e0cee191a465cbd4bc60127b
0xdiab <islamdiab967@gmail.com>
2025-04-17 06:50:08 +0200

    create a footer component

---
+++ resources/views/include/footer.blade.php
@@ -0,0 +1,9 @@
+{{-- Footer --}}
+<footer class="footer">
+    {{-- Container --}}
+    <div class="container">
+        <p class="p-0 m-0 text-center">&copy; All Rights are reserved <a href="">GuidanceHub</a>.</p>
+    </div>
+    {{-- End of Container --}}
+</footer>
+{{-- End of Footer --}}
