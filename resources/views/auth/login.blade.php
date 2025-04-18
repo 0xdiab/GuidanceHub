373b83a8e8f434194ddf719b886e89ef4397f90c
0xdiab <islamdiab967@gmail.com>
2025-04-18 07:02:37 +0200

    add register link for any guest want to register in app

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,64 +6,68 @@
 @endsection
 
 {{-- remove navbar --}}
-@section('navbar') @endsection
+@section('navbar')
+@endsection
 
 {{-- remove footer --}}
-@section('footer') @endsection
+@section('footer')
+@endsection
 
 {{-- Content --}}
-@section("content")
-<section class="login-section sign-section d-flex justify-content-center py-4">
-    {{-- Login Form --}}
-    <div class="login-form">
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-            <!-- Email Address -->
-            <div>
-                <label class="d-block" for="email">Email</label>
-                <input id="email" class="block mt-1 w-full" type="email" name="email" value="{{ old('email') }}" required autofocus autocomplete="username" />
-                <error :messages="$errors->get('email')" class="mt-2" />
-            </div>
-        
-            <!-- Password -->
-            <div class="mt-4">
-                <label class="d-block" for="password">Password</label>
-                <input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-        
-                <error :messages="$errors->get('password')" class="mt-2" />
-            </div>
-        
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                    <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-        
-            <div class="flex items-center justify-end mt-4">
-                {{-- @if (Route::has('password.request'))
+@section('content')
+    <section class="login-section sign-section d-flex justify-content-center py-4">
+        <div class="content d-flex justify-content-center align-items-center">
+            {{-- Login Form --}}
+            <div class="login-form">
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <!-- Email Address -->
+                    <div>
+                        <label class="d-block" for="email">Email</label>
+                        <input id="email" class="form-control" type="email" name="email"
+                            value="{{ old('email') }}" required autofocus autocomplete="username" placeholder="Type your email" />
+                        <error :messages="$errors - > get('email')" class="mt-2" />
+                    </div>
+
+                    <!-- Password -->
+                    <div class="mt-4">
+                        <label class="d-block" for="password">Password</label>
+                        <input id="password" class="form-control" type="password" name="password" required
+                            autocomplete="current-password" placeholder="Type your password" />
+
+                        <error :messages="$errors - > get('password')" class="mt-2" />
+                    </div>
+
+                    <div class="d-flex align-items-center my-4">
+                        {{-- @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif --}}
-        
-                <button class="ms-3 btn btn-login" type="submit">
-                    {{ __('Log in') }}
-                </button>
-            </div>
-        </form>
-    </div>
-    {{-- End of Login Form --}}
 
-    {{-- Login Content --}}
-    <div class="login-content">
-        <h1 class="font-bold">Welcome!, back to the <span>GuidanceHub</span></h1>
-    </div>
-    {{-- End of Login Content --}}
-</section>
+                        <button class="btn btn-login" type="submit">
+                            {{ __('Log in') }}
+                        </button>
+                    </div>
+                </form>
+
+                <p>Not a Member? <a class="" href="{{ route('register') }}">{{ __('Register Now') }}</a></p>
+            </div>
+            {{-- End of Login Form --}}
+
+            {{-- Login Content --}}
+            <div class="login-content text-center">
+                <div class="icon">
+                    <i class="fa-brands fa-connectdevelop"></i>
+                </div>
+                <div class="text">
+                    <h1 class="m-0">Welcome!, back to the <span>GuidanceHub</span></h1>
+                </div>
+                
+            </div>
+            {{-- End of Login Content --}}
+        </div>
+
+    </section>
 @endsection
 {{-- End of Content --}}
