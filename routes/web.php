f17b1a754f619f4031dd85764bfce3e2186a667f
0xdiab <islamdiab967@gmail.com>
2025-04-22 06:19:03 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Dashboard\AdminHomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,18 +8,18 @@ Route::get('/', function () {
     return view('Pages.home');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
- 
+require __DIR__ . '/auth.php';
+require __DIR__ . '/dashboard/web.php';
 
-
+// Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.', 'middleware' => ['auth', 'is_admin']], function() {
+    
+//     // Admin home
+//     Route::get('/', [AdminHomeController::class, 'index'])->name('dashboard.home');
+// });
 
