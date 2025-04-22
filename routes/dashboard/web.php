1217c72e488dd40806b09e202a8668112978acaf
0xdiab <islamdiab967@gmail.com>
2025-04-22 06:18:58 +0200

    add home route to dashboard

--- routes/dashboard/web.php
+++ routes/dashboard/web.php
@@ -1,7 +1,16 @@
 <?php
 
-use Illuminate\Routing\Route;
+use App\Http\Controllers\Dashboard\AdminHomeController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('dashboard', function(){
-
+Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.', 'middleware' => ['auth', 'is_admin']], function() {
+    
+    // Admin home
+    Route::get('/', [AdminHomeController::class, 'index'])->name('home');
 });
+    
+
+
+// Route::middleware(['auth', 'is_admin'])->group(function () {
+// });
+
