ee6d27e5b9103fe2e9d53e9e22c52137f40e5e13
0xdiab <islamdiab967@gmail.com>
2025-04-23 15:22:01 +0200

    write create method to return create skills page

--- app/Http/Controllers/dashboard/SkillController.php
+++ app/Http/Controllers/dashboard/SkillController.php
@@ -22,7 +22,7 @@ class SkillController extends Controller
      */
     public function create()
     {
-        //
+        return view('dashboard.skills.create');
     }
 
     /**
@@ -30,7 +30,8 @@ class SkillController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        dd($request);
+
     }
 
     /**
