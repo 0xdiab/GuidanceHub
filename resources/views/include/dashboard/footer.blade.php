f0400fd4af771fd2c43942b47c7cb9730dfc6294
0xdiab <islamdiab967@gmail.com>
2025-04-23 14:06:25 +0200

    create a footer component

---
+++ resources/views/include/dashboard/footer.blade.php
@@ -0,0 +1,11 @@
+{{-- Footer --}}
+<footer class="footer">
+    {{-- Container --}}
+    <div class="container">
+        <div class="footer-info d-flex justify-content-center">
+            <p class="p-0 m-0 text-center">&copy; All Rights are reserved <a href="">GuidanceHub</a>.</p>
+        </div>
+    </div>
+    {{-- End of Container --}}
+</footer>
+{{-- End of Footer --}}
