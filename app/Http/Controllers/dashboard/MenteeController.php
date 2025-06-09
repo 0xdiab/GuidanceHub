bd60fcf3c5852435be83752f5909c476da42825b
0xdiab <islamdiab967@gmail.com>
2025-06-09 16:27:09 +0300

    write four methods for create, store, edit, update

--- app/Http/Controllers/dashboard/MenteeController.php
+++ app/Http/Controllers/dashboard/MenteeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Dashboard;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class MenteeController extends Controller
 {
@@ -22,7 +23,7 @@ class MenteeController extends Controller
      */
     public function create()
     {
-        //
+        return view("dashboard.mentees.create");
     }
 
     /**
@@ -30,7 +31,22 @@ class MenteeController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // dd($request);
+        User::create([
+            "name"              => $request['name'],
+            "email"             => $request['email'],
+            "password"          => Hash::make($request['password']),
+            "position"          => $request['position'],
+            "session_price"     => $request['session_price'],
+            "linkedin_url"      => $request['linkedin_url'],
+            "x_url"             => $request['twitter_url'],
+            "github_url"        => $request['github_url'],
+            "cv_url"            => $request['cv_url'],
+            "gender"            => $request['gender'],
+            "account_type"      => ($request['account_type'] == 1) ? "mentor" : "mentee",
+        ]);
+
+        return redirect()->route('dashboard.mentees.store');
     }
 
     /**
@@ -46,7 +62,8 @@ class MenteeController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $mentee = User::where('account_type', 'mentee')->find($id);
+        return view("dashboard.mentees.update", compact('mentee'));
     }
 
     /**
@@ -54,7 +71,23 @@ class MenteeController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        // dd($request);
+        $mentee = User::where('account_type', 'mentee')->find($id);
+        $mentee->update([
+            "name"              => $request['name'],
+            "email"             => $request['email'],
+            "password"          => Hash::make($request['password']),
+            "position"          => $request['position'],
+            "session_price"     => $request['session_price'],
+            "linkedin_url"      => $request['linkedin_url'],
+            "x_url"             => $request['twitter_url'],
+            "github_url"        => $request['github_url'],
+            "cv_url"            => $request['cv_url'],
+            "gender"            => $request['gender'],
+            "account_type"      => ($request['account_type'] == 1) ? "mentor" : "mentee",
+        ]);
+
+        return view("dashboard.mentees.update", compact('mentee'));
     }
 
     /**
