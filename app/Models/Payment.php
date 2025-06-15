4cb74b70d60ad0b6d17598d4cb4daf52cd1f09c0
0xdiab <islamdiab967@gmail.com>
2025-06-15 16:43:23 +0300

    add all attributes in fillable

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    protected $faillable =[
+        'mentor_id',
+        'mentee_id',
+        'session_time',
+        'session_link',
+        'status',
+        'is_paid',
+        'payment_id',
+        'meeting_provider',
+        'meeting_id'
+    ];
 }
