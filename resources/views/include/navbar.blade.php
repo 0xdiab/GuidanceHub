2b432ed46c3dc2a86844800d45c1ee214fb6cc38
0xdiab <islamdiab967@gmail.com>
2025-06-20 18:45:49 +0300

    add mentor,mentee routes

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -12,13 +12,13 @@
             {{-- Navbar Links --}}
             <ul class="navbar-nav m-auto mb-2">
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route("user.home") }}">Home</a>
+                    <a class="nav-link" href="{{ route('user.home') }}">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('user.specialization.index') }}">Specializations</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route("user.about") }}">About Us</a>
+                    <a class="nav-link" href="{{ route('user.about') }}">About Us</a>
                 </li>
                 {{-- <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
@@ -36,51 +36,58 @@
                 </li> --}}
             </ul>
             {{-- End of Navbar Links --}}
-                @guest
-                    <div class="buttons">
-                        <ul class="m-0 navbar-nav m-auto">
-                            <li class="nav-item me-2">
-                                <a class="nav-link btn btn-nav btn-login" href="{{ route('login') }}">Login</a>
-                            </li>
-                            <li class="nav-item ms-2">
-                                <a class="nav-link btn btn-nav btn-register" href="{{ route('register') }}">Register</a>
-                            </li>
-                        </ul>
-                    </div>
-                @else
-                    <ul class="profile px-4">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="profile" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                {{ Auth::user()->name }}
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="profile">
-                                @auth
-                                    @if ((Auth::user()->is_admin))
-                                        <li><a class="dropdown-item" href="{{ route('dashboard.home') }}">Admin Dashboard</a></li>
-                                    @endif
-                                @endauth
-                                <li><a class="dropdown-item" href="{{route('profile.index')}}">Profile</a></li>
-                                <li><a class="dropdown-item" href="{{route('profile.edit')}}">Settings</a></li>
-                                {{-- <li><a class="dropdown-item" href="#">Another action</a></li> --}}
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                            document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                        class="d-none">
-                                        @csrf
-                                    </form>
-                                </li>
-                            </ul>
+            @guest
+                <div class="buttons">
+                    <ul class="m-0 navbar-nav m-auto">
+                        <li class="nav-item me-2">
+                            <a class="nav-link btn btn-nav btn-login" href="{{ route('login') }}">Login</a>
+                        </li>
+                        <li class="nav-item ms-2">
+                            <a class="nav-link btn btn-nav btn-register" href="{{ route('register') }}">Register</a>
                         </li>
                     </ul>
-                @endguest
+                </div>
+            @else
+                <ul class="profile px-4">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="profile" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            {{ Auth::user()->name }}
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="profile">
+                            @auth
+                                @if (Auth::user()->is_admin)
+                                    <li><a class="dropdown-item" href="{{ route('dashboard.home') }}">Admin Dashboard</a></li>
+                                @endif
+                            @endauth
+                            <li><a class="dropdown-item" href="{{ route('profile.index') }}">Profile</a></li>
+                            @if (Auth::user()->account_type == 'mentor')
+                                <li><a class="dropdown-item" href="{{ route('sessions.mentorSessions', Auth::user()->id) }}">My Session</a></li>
+                            @endif
+
+                            @if (Auth::user()->account_type == 'mentee')
+                                <li><a class="dropdown-item" href="{{ route('sessions.menteeSessions', Auth::user()->id) }}">My Session</a></li>
+                            @endif
+                            
+                            <li><a class="dropdown-item" href="{{ route('profile.edit') }}">Settings</a></li>
+                            {{-- <li><a class="dropdown-item" href="#">Another action</a></li> --}}
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            @endguest
         </div>
     </div>
     {{-- End of  Container --}}
