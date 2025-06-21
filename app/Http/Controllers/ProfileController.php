df6315ba9ec561ed5d822702c56806f55d6bd27d
0xdiab <islamdiab967@gmail.com>
2025-06-21 04:06:46 +0300

    return all details for user, and write update method

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,25 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\MentorSession;
+use App\Models\Specialization;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
     public function index()
     {
-        return view('profile.index');
+        $user = Auth::user();
+        return view('profile.index', compact('user'));
     }
     /**
      * Display the user's profile form.
      */
     public function edit(Request $request): View
     {
+        $specializations = Specialization::all();
+
         return view('profile.edit', [
             'user' => $request->user(),
+            'specializations' => $specializations
         ]);
     }
 
@@ -30,13 +37,36 @@ class ProfileController extends Controller
      */
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
+        // dd($request->user());
         $request->user()->fill($request->validated());
 
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
         }
 
-        $request->user()->save();
+        $request->user()->update([
+            "name"              => $request['name'],
+            "email"             => $request['email'],
+            "password"          => Hash::make($request['password']),
+            "position"          => $request['position'],
+            "session_price"     => $request['session_price'],
+            "linkedin_url"      => $request['linkedin_url'],
+            "x_url"             => $request['twitter_url'],
+            "github_url"        => $request['github_url'],
+            "cv_url"            => $request['cv_url'],
+            "gender"            => $request['gender'],
+            "account_type"      => ($request['account_type'] == 1) ? "mentor" : "mentee",
+        ]);
+
+
+        if ($request->has('specializations')) {
+            $request->user()->specializations()->sync($request->specializations);
+        }
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image')->store('profile_images', 'public');
+            $request->user()->update(['image' => basename($image)]);
+        }
 
         return Redirect::route('profile.edit')->with('status', 'profile-updated');
     }
